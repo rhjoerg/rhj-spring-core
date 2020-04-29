5f04824007b99273916e449cd6593f928b380349
rhjoerg <rhjoerg@gmail.com>
2020-04-29 06:40:02 +0200

    added some resource support

--- src/main/java/ch/rhj/spring/io/Resources.java
+++ src/main/java/ch/rhj/spring/io/Resources.java
@@ -1,7 +1,40 @@
 package ch.rhj.spring.io;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 /**
  * @author Roger H. Jörg
  */
 public class Resources {
+
+	public static ClassPathResource classPathResource(String path) {
+
+		return new ClassPathResource(path);
+	}
+
+	public static byte[] resourceToBytes(Resource resource) throws IOException {
+
+		try (InputStream input = resource.getInputStream()) {
+
+			return IOUtils.toByteArray(input);
+		}
+	}
+
+	public static String resourceToString(Resource resource, Charset charset) throws IOException {
+
+		return new String(resourceToBytes(resource), charset);
+	}
+
+	public static String resourceToString(Resource resource) throws IOException {
+
+		return resourceToString(resource, UTF_8);
+	}
 }
--- src/test/java/ch/rhj/spring/io/ResourcesTests.java
+++ src/test/java/ch/rhj/spring/io/ResourcesTests.java
@@ -1,5 +1,9 @@
 package ch.rhj.spring.io;
 
+import static ch.rhj.spring.io.Resources.classPathResource;
+import static ch.rhj.spring.io.Resources.resourceToString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class ResourcesTests {
@@ -8,5 +12,14 @@ public class ResourcesTests {
 	public void testCtor() {
 
 		new Resources();
+	}
+
+	@Test
+	public void testResourceToString() throws Exception {
+
+		String expected = "hello, world!";
+		String actual = resourceToString(classPathResource("/hello.txt"));
+
+		assertEquals(expected, actual);
 	}
 }
